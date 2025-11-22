ef5526fa0bdf53960caa97e7c0c6be98f483fc9f
Olumide Daramola <96479799+olumidedaramola21@users.noreply.github.com>
2025-11-23 00:48:45 +0100

    feat: update

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: '',
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+  }
+}
