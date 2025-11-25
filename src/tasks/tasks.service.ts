9b44dcd07330275bd0569d72a10f9f6ccba13ffb
Olumide Daramola <96479799+olumidedaramola21@users.noreply.github.com>
2025-11-25 09:35:49 +0100

    feat(tasks.service): implement filtering logic in the service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -19,6 +20,25 @@ export class TasksService {
       throw new NotFoundException(`Tasks with ID "${id}" not found`);
     }
     return found;
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = this.tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = this.tasks.filter(
+        (tasks) =>
+          tasks.title.includes(search) || tasks.description.includes(search),
+      );
+    }
+
+    return tasks;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
